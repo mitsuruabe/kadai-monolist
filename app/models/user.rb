cbd48e4bff71c3735f8d61d384d4563f02c4b9f9
mitsuruabe <abem0146@gmail.com>
2017-04-24 06:31:36 +0000

    add have

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
   has_many :ownerships
   has_many :items, through: :ownerships
   
+  #-------------------------------------------------------------------------
   has_many :wants
   has_many :want_items, through: :wants, class_name: 'Item', source: :item
   
@@ -23,5 +24,22 @@ class User < ApplicationRecord
 
   def want?(item)
     self.want_items.include?(item)
+  end
+  
+ #-------------------------------------------------------------------------
+  has_many :haves, class_name: 'Have'
+  has_many :have_items, through: :haves, class_name: 'Item', source: :item  
+
+  def have(item)
+    self.haves.find_or_create_by(item_id: item.id)
+  end
+
+  def unhave(item)
+    have = self.haves.find_by(item_id: item.id)
+    have.destroy if have
+  end
+
+  def have?(item)
+    self.have_items.include?(item)
   end
 end
